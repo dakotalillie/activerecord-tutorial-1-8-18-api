cc6f4683b2f05b6aa9521d2c41017e2185ea8408
Dakota Lillie <dakota.lillie@icloud.com>
2018-01-07 13:20:59 -0500

    heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins ['https://actioncable-tutorial-dom.herokuapp.com', 'http://actioncable-tutorial-dom.herokuapp.com']
 
     resource '*',
       headers: :any,
